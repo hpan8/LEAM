409dc001e46a0efa7fa5a380b3969e481997e94f
Youshan <helen.youshan@gmail.com>
2016-03-30 16:44:07 -0500

    landcover and road speed without selecting max

--- landcover2speedmap.py
+++ landcover2speedmap.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python
-import os
-import sys
-sys.path += ['./bin']
-import re
-import csv
-import subprocess
-from glob import iglob
-from subprocess import check_call
-from grasssetup import grass_config
-from grasssetup import import_rastermap
+from StringIO import StringIO
+import numpy as np
+import pandas as pd
+from pandas import (Series,DataFrame, Panel,)
+from pprint import pprint
 
-LANDCOVER='./LU2Travel_Speed_Pan/LU2006ASCII/landcover2006.txt'
+
+# overlap two dataframes
+#http://stackoverflow.com/questions/26876549/in-python-pandas-numpy-how-to-create-a-column-with-the-max-min-value-from-two
+# using np.maxium
+
+#LANDCOVER='./LU2Travel_Speed_Pan/LU2006ASCII/landcover2006.txt'    #ascii map
+#ROAD='./LU2Travel_Speed_Pan/LU2006ASCII/chrdras.txt'               #ascii map
+LANDCOVER='./LU2Travel_Speed_Pan/LU2006ASCII/landcover2006-cut.txt'
+ROAD='./LU2Travel_Speed_Pan/LU2006ASCII/chrdras-cut.txt'
 SPEEDCHART = "./LU2Travel_Speed_Pan/nlcd_1992+lu2travel_speed.txt"
 
+def asciiMap2DataFrame(file):
+    return pd.read_csv(file, skiprows=6, header=None, sep=r"\s+")
+    
 class SpeedMap:
-
-	def __init__(self, landcover, speedchart):
-		self.landcovermap = landcover
-		self.speed_dict = {}
-		self.speedchart2dict(speedchart)
-		self.landcover2speedmap(landcover)
-
-	def speedchart2dict(self, speedchart):
-		with open(speedchart, 'r') as f:
-			next(f) #skip the header row
-			for line in f:
-				(cat, speed) = line.split(',')
-				self.speed_dict[cat] = speed
-
-	def printdict(self, dict):
+    def __init__(self, landcover_matrix, road_matrix, speedchart):
+        self.cat_list = []
+        self.speed_list = []
+        # read the speedchart to be a speed dictionary
+        self.speedchart2dict(speedchart)
+        # replace the landcover catogory values with speed values
+        self.landcover_matrix = landcover_matrix
+        self.landcoverspeed_matrix = self.cat2speedmap(landcover_matrix)
+        self.roadspeed_matrix = self.cat2speedmap(road_matrix)
+        pprint(self.landcoverspeed_matrix)
+        pprint(self.roadspeed_matrix)
+    
+    def speedchart2dict(self, speedchart):
+        with open(speedchart, 'r') as f:
+            next(f) #skip the header row
+            for line in f:
+                (cat, speed) = line.split(',')
+                speed = speed.split('\r\n')[0] #get rid of the \r\n appended to each speed
+                self.cat_list.append(int(cat))
+                self.speed_list.append(int(speed))
+    
+    def printdict(self, dict):
 		for key, value in dict.iteritems():
 			print str(key) + ":"+ str(value)
-
-	def landcover2speedmap(self, landcover):
-		with open(landcover, 'r') as f:
-			inmatrix = [map(int, line.split()) for line in f.readlines()[6:]]
-			modification = 0
-			for line in inmatrix:
-				for cat in line:
-					modification = 1
-				if modification:
-					print line
-
-
+    
+    def cat2speedmap(self, matrix):
+        return matrix.replace(to_replace=self.cat_list, value=self.speed_list)
+        
+        
 def main():
-    grass_config('grass', 'model')
-    #import_rastermap('landcover')
-    speedmap = SpeedMap(LANDCOVER, SPEEDCHART)
+    landcover_matrix = asciiMap2DataFrame(LANDCOVER)
+    road_matrix = asciiMap2DataFrame(ROAD)
+    speedmap = SpeedMap(landcover_matrix, road_matrix, SPEEDCHART)
     #speedmap.printdict(speedmap.speed_dict)
 
 
